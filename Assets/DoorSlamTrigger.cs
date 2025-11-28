2b4e9ed4eeb52ebb8ebf0882fc4263aac3211fec
Terloaf <greenyryan2501@gmail.com>
2025-11-27 22:24:36 -0400

    Fixed overlapping noise

--- Assets/DoorSlamTrigger.cs
+++ Assets/DoorSlamTrigger.cs
@@ -2,15 +2,22 @@ using UnityEngine;
 
 public class DoorSlamTrigger : MonoBehaviour
 {
+    public GameObject Timeline;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Timeline.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Timeline.gameObject.SetActive(true);
+
     }
 }
